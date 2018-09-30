f4d6f2a88262e60d941f2a7d0404c8b17577c8c2
“SivonneWard” <sivonne@live.unc.edu>
2018-09-30 15:30:05 -0400

    working on adding JS to match the HTML boxes

---
+++ js/scripts.js
@@ -0,0 +1,109 @@
+/*
+Write a function that buids an HTML table with the data below. The table should have four columns:
+first name, last name, position, and year. If the player is a Senior, her information should be in bold.
+*/
+
+var first = 
+var players = [
+  {
+    first:'Angela',
+    last: 'Smith',
+    position: 'Guard',
+    year: 'Junior'
+  },
+    {
+    first:'Ashley',
+    last: 'Roberts',
+    position: 'Guard',
+    year: 'Senior'
+  },
+    {
+    first:'Sarah',
+    last: 'Spangler',
+    position: 'Forward',
+    year: 'Freshman'
+  },
+   {
+    first:'Catherine',
+    last: 'McAllister',
+    position: 'Forward',
+    year: 'Senior'
+  },
+   {
+    first:'Haley',
+    last: 'Bishop',
+    position: 'Center',
+    year: 'Sophomore'
+  },
+  {
+   first:'Renee',
+   last: 'Williams',
+   position: 'Guard',
+   year: 'Senior'
+ },
+ {
+  first:'Jennifer',
+  last: 'Olsen',
+  position: 'Forward',
+  year: 'Junior'
+  }
+];
+//Write your function here
+function playerTable{
+  for(i = 0; i < players.length; i++){
+    first = document.getElementById('col1');
+    last = document.getElementById('col2');
+    position = document.getElementById('col3');
+    year = document.getElementById('col4');
+  }
+  }
+playerTable();
+/*
+EXTRA CREDIT CHALLENGE (5 POINTS): Write a function that compares the list above with the list below, finds the players that made the All-State team, and displays a message with the results: "Congratulations to Springfield's 2018 North Carolina All-State honorees: ____." Display the message in a div below the table.
+Hint: You need two loops, one of which will be 'nested'.*/
+/*var allStars = [
+  {
+    first:'Melanie',
+    last: 'Akers',
+    position: 'Forward',
+    school:'Johnson High School'
+  },
+    {
+    first:'Olivia',
+    last: 'Carter',
+    position: 'Foward',
+    school:'Providence Day School'
+  },
+    {
+    first:'Sarah',
+    last: 'Spangler',
+    position: 'Forward',
+    school:' Springfield High School'
+  },
+   {
+    first:'Ursula',
+    last: 'Jones',
+    position: 'Guard',
+    school: 'Chatham Senior High School'
+  },
+   {
+    first:'Theresa',
+    last: 'Hollinger',
+    position: 'Guard',
+    school: 'St. Joseph Academy'
+  },
+  {
+   first:'Darleen',
+   last: 'Lawrence',
+   position: 'Guard',
+   school: 'Everton High School'
+ },
+ {
+  first:'Jennifer',
+  last: 'Olsen',
+  position: 'Forward',
+  school: 'Springfield High School'
+  }
+];
+
+https://www.w3schools.com/js/js_htmldom_html.asp
