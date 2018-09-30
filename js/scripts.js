7398bbd5852f9b14e493bc12cac06440a8b8c518
“SivonneWard” <sivonne@live.unc.edu>
2018-09-30 17:06:03 -0400

    tried to consolidate code (did not realize we weren't supposed to add to the actual HTML, now there are rows and columns, still need to bold Seniors

--- js/scripts.js
+++ js/scripts.js
@@ -46,26 +46,38 @@ var players = [
   year: 'Junior'
   }
 ];
+var res = document.getElementById('table-content');
+var html = '';
 //Write your function here
 function playerTable(){
-
 for(i = 0; i < players.length; i++){
-    /*document.getElementById('col1').innerHTML = players.first;
-    document.getElementById('col2').innerHTML = players.last;
-    document.getElementById('col3').innerHTML = players.position;
-    document.getElementById('col4').innerHTML = players.year;*/
+      html += '<td>' + players[i].first;
+      html += '<td>' + players[i].last ;
+      html += '<td>' + players[i].position;
+      html += '<td>' + players[i].year + '<tr>'+ '</tr>';
+      html += '</td>';
+    }
+    res.innerHTML = '<table>' + html + '</table>';
+}
 
+playerTable();
+/*for(i = 0; i < players.length; i++){
     document.getElementById('col1').innerHTML += '<td>' + players[i].first;
     document.getElementById('col2').innerHTML += '<td>' + players[i].last;
     document.getElementById('col3').innerHTML += '<td>' + players[i].position;
     document.getElementById('col4').innerHTML+= '<td>' + players[i].year;
     document.getElementById('col4').innerHTML+= '</td>';
-
-
+  }
+  if (players.year == 'Senior'){
+     year.bold();
+     tr.first.bold();
+     tr.last.bold();
+     tr.position.bold();
   }
 
-}
-playerTable();
+}*/
+
+
 /*
 EXTRA CREDIT CHALLENGE (5 POINTS): Write a function that compares the list above with the list below, finds the players that made the All-State team, and displays a message with the results: "Congratulations to Springfield's 2018 North Carolina All-State honorees: ____." Display the message in a div below the table.
 Hint: You need two loops, one of which will be 'nested'.*/
@@ -117,4 +129,5 @@ Hint: You need two loops, one of which will be 'nested'.*/
 /*
 https://www.w3schools.com/js/js_htmldom_html.asp
 https://www.w3schools.com/js/js_htmldom_elements.asp
+https://www.w3schools.com/js/js_htmldom_css.asp
 */
